82fbe3dea7c03d77468f2c0ac5b1d01b4958370a
Ryan Lee <yklee0916@gmail.com>
2017-03-30 00:39:12 +0900

    [Ryan Lee] add upside down orientation

---
+++ Podic/Categories/UIViewController+UpsideDown.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+UpsideDown.h
+//  Podic
+//
+//  Created by Rio on 30/03/2017.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIViewController (UpsideDown)
+
+@end
---
+++ Podic/Categories/UIViewController+UpsideDown.m
@@ -0,0 +1,17 @@
+//
+//  UIViewController+UpsideDown.m
+//  Podic
+//
+//  Created by Rio on 30/03/2017.
+//  Copyright © 2017 Ryan Lee. All rights reserved.
+//
+
+#import "UIViewController+UpsideDown.h"
+
+@implementation UIViewController (UpsideDown)
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskAll;
+}
+
+@end
